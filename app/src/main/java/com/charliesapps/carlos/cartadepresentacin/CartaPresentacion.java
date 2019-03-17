8535ab0488951ef30dc614fb680efd684f1b43b8
CarlosBeltranP <00507511@uca.edu.sv>
2019-03-17 11:04:09 -0600

    Finalizado! Solucion a error por imagen jpg

--- app/src/main/java/com/charliesapps/carlos/cartadepresentacin/CartaPresentacion.java
+++ app/src/main/java/com/charliesapps/carlos/cartadepresentacin/CartaPresentacion.java
@@ -2,12 +2,15 @@ package com.charliesapps.carlos.cartadepresentacin;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class CartaPresentacion extends AppCompatActivity {
+    private TextView texto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_carta_presentacion);
+        texto = (TextView) findViewById(R.id.text);
     }
 }
